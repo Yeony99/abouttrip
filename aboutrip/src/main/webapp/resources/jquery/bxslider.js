19caf1a0664a28ad71374cd85addbdca599b8832
Yeony99 <ny.yeony.kim@gmail.com>
2021-05-31 22:34:43 +0900

    Add images for slider

---
+++ aboutrip/src/main/webapp/resources/jquery/bxslider.js
@@ -0,0 +1,11 @@
+  $(document).ready(function() {
+	$('.bxslider').bxSlider({ // 클래스명 주의!
+		auto: true,
+		speed: 500, 
+		pause: 7000,  
+		mode: 'horizontal', 
+		autoControls: false, 
+		pager: false, 
+		captions: false
+	});
+});
