3bf0729eacce1ec75f2b6023640661a9cabab0d3
Jaeromi <Jaeromi@118.37.192.233>
2021-06-02 16:49:13 +0900

    커밋 많이 하지 말라했던거같은데

--- aboutrip/src/main/java/com/aboutrip/app/admin/main/MainController.java
+++ aboutrip/src/main/java/com/aboutrip/app/admin/main/MainController.java
@@ -1,15 +1,23 @@
 package com.aboutrip.app.admin.main;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aboutrip.app.member.SessionInfo;
 
 @Controller("admin.mainController")
 @RequestMapping(value = "/admin/*")
 public class MainController {
 	
 	@RequestMapping(value="main", method=RequestMethod.GET)
-	public String admin() throws Exception {
+	public String admin(HttpSession session) throws Exception {
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		if(info.getUserNum()!=1111) {
+			return "redirect:/member/main";
+		}
 		return ".admin.main.main";
 	}
 }
