ea962dcd49e9c9ab4ef65af4436292ca716eff60
Yeony99 <ny.yeony.kim@gmail.com>
2021-06-02 22:02:07 +0900

    Update PlaceController to request mdPick.jsp

--- aboutrip/src/main/java/com/aboutrip/app/place/PlaceController.java
+++ aboutrip/src/main/java/com/aboutrip/app/place/PlaceController.java
@@ -3,13 +3,19 @@ package com.aboutrip.app.place;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller("place.placeController")
 @RequestMapping("/place/*")
 public class PlaceController {
 	
-	@RequestMapping(value="list", method=RequestMethod.GET)
-	public String placelist() throws Exception{
-		return ".place.list";
+	@RequestMapping("list")
+	public ModelAndView placelist() throws Exception{
+		return new ModelAndView(".place.list");
+	}
+	
+	@RequestMapping("mdPick")
+	public ModelAndView mdPick() throws Exception{
+		return new ModelAndView(".place.mdPick");
 	}
 }
