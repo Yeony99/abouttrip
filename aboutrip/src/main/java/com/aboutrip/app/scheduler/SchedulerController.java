222362cb2aad8c9424d2cb0b449feffadb325adc
Yeony99 <ny.yeony.kim@gmail.com>
2021-06-15 14:35:24 +0900

    Fix SchedulerController.java @Controller to @RestController

--- aboutrip/src/main/java/com/aboutrip/app/scheduler/SchedulerController.java
+++ aboutrip/src/main/java/com/aboutrip/app/scheduler/SchedulerController.java
@@ -7,16 +7,16 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.aboutrip.app.member.SessionInfo;
 
-@Controller("scheduler.schedulerController")
+@RestController("scheduler.schedulerController")
 @RequestMapping("/scheduler/*")
 public class SchedulerController {
 
